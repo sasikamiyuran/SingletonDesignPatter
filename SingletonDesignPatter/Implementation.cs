5b89369fc6e7993402b031afe496f3f5a8ebb40f
sasikamiyuran <sasikamiyuran@gmail.com>
2024-02-27 11:14:33 +0530

    Initiate singleton pattern

---
+++ SingletonDesignPatter/Implementation.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SingletonDesignPatter
+{
+    /// <summary>
+    /// Singleton
+    /// </summary>
+    public class Logger
+    {
+        private static Logger? _instance;
+
+        /// <summary>
+        /// Instence
+        /// </summary>
+        public static Logger Instance
+        {
+            get
+            {
+                if( _instance == null)
+                {
+                    _instance = new Logger();
+                }
+                return _instance;
+            }
+        }
+
+        protected Logger() { }
+
+        public void Log(string message)
+        {
+            Console.WriteLine($"Message to log: {message}");
+        }
+    }
+}
--- SingletonDesignPatter/Program.cs
+++ SingletonDesignPatter/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using SingletonDesignPatter;
+
+Console.Title = "Singleton Design Pattern";
+
+//call the proterty getter twice
+var instence1 = Logger.Instance;
+var instence2 = Logger.Instance;
+
+if(instence1 == instence2 && instence1 == Logger.Instance)
+{
+    Console.WriteLine("Instence are same");
+}
+
+instence1.Log($"Message from {nameof(instence1)}");
+instence2.Log($"Message from {nameof(instence2)}");
+Logger.Instance.Log($"Message from {nameof(Logger.Instance)}");
+
+Console.ReadLine();
